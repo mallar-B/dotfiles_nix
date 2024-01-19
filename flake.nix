06bfad53de281b5c3f4fe93dc3c8502b6498f2f2
mallar-B <bhattacharyamallar@gmail.com>
2024-01-19 05:14:48 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+
+  description = "first flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";  
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in{
+    nixosConfigurations = {
+      MAIN = lib.nixosSystem {
+        inherit system;
+	modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      mallarb = home-manager.lib.homeManagerConfiguration{
+	inherit pkgs;
+	modules = [ ./home.nix ];	
+      };
+    };
+  };
+
+}
