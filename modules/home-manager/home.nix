40aaedaeff5b3e1cbb1a1dc7f2d8337fe92378c0
mallar-B <bhattacharyamallar@gmail.com>
2024-10-24 17:01:21 +0530

    temp

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -7,20 +7,13 @@
   home.stateVersion = "24.05";
 
     home.packages = with pkgs; [
-
+      gbar
+      hypridle
+      hyprlock
+      hyprpaper
     ];
 
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
     ".config/hypr".source = ../../configs/hypr;
     ".config/nvim".source = ../../configs/nvim;
     ".config/foot".source = ../../configs/foot;
@@ -28,25 +21,14 @@
     # ".config/mpv".source = ../../configs/mpv;
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/mallarb/etc/profile.d/hm-session-vars.sh
-  #
+
   home.sessionVariables = {
     # EDITOR = "emacs";
+    TERM = "foot";
   };
+fonts.packages = with pkgs; [
+  (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+];
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
