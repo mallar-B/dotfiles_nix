b084769800866f0fc71f2c75574cf4943df0dfcf
mallar-B <bhattacharyamallar@gmail.com>
2025-03-05 14:45:04 +0530

    update neotree

--- modules/nixvim/config/bufferline.nix
+++ modules/nixvim/config/bufferline.nix
@@ -1,8 +1,42 @@
-{
+{pkgs,self, ...}: {
   plugins = {
-    bufferline.enable = true;
-    web-devicons.enable = true;
-    neo-tree{
+    telescope.enable = true;
+    gitsigns.enable = true;
+    which-key.enable = true;
+    lazydev.enable = true;
+    treesitter.enable = true;
+    nvim-autopairs.enable = true;
+    dap.enable = true;
+    indent-blankline.enable = true;
+    lint.enable = true;
+    cmp = {
       enable = true;
+      autoEnableSources = false;
+      settings.sources = [
+	{ name = "nvim_lsp"; }
+	{ name = "path"; }
+	{ name = "buffer"; }
+      ];
+    };
+    cmp-nvim-lsp.enable = true;
+    cmp-path.enable = true;
+    cmp-buffer.enable = true;
+    conform-nvim.enable = true;
+    lsp = {
+      enable = true;
+      servers = {
+	ts_ls.enable = true;
+	tailwindcss.enable = true;
+      };
+    };
   };
+  extraPlugins = with pkgs.vimPlugins; [
+    {
+      plugin = gruvbox-material;
+      config = ''
+	lua vim.cmd.colorscheme('gruvbox-material')
+	lua vim.cmd.colorscheme('gruvbox-material') 
+      '';
+    }
+  ];
 }
---
+++ modules/nixvim/config/plugins/neo-tree.nix
@@ -0,0 +1,13 @@
+{
+plugins = {
+  web-devicons.enable = true;
+  neo-tree = {
+    enable = true;
+    extraOptions = {
+      window = {
+        position = "right";
+      };
+    };
+  };
+};
+}
