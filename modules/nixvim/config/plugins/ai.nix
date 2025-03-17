24c968ae8e3d29778dcfd0eea115aef46f62f352
mallar-B <bhattacharyamallar@gmail.com>
2025-03-17 19:31:49 +0530

    changed ai agent to gemini in nixvim

--- modules/nixvim/config/plugins/ai.nix
+++ modules/nixvim/config/plugins/ai.nix
@@ -1,17 +1,41 @@
 {
-  lib,
-  pkgs,
-  ...
-}: {
-  plugins = {
-    codeium-nvim.enable = true;
-    codeium-nvim.settings = {
-      enable_chat = true;
-      tools = {
-        curl = lib.getExe pkgs.curl;
-        gzip = lib.getExe pkgs.gzip;
-        uname = lib.getExe' pkgs.coreutils "uname";
-        uuidgen = lib.getExe' pkgs.util-linux "uuidgen";
+  # plugins = {
+  #   codeium-nvim.enable = true;
+  #   codeium-nvim.settings = {
+  #     enable_chat = true;
+  #     tools = {
+  #       curl = lib.getExe pkgs.curl;
+  #       gzip = lib.getExe pkgs.gzip;
+  #       uname = lib.getExe' pkgs.coreutils "uname";
+  #       uuidgen = lib.getExe' pkgs.util-linux "uuidgen";
+  #     };
+  #   };
+  # };
+  plugins.codecompanion.enable = true;
+  plugins.codecompanion.settings = {
+    adapters = {
+      gemini = {
+        __raw = ''
+          function()
+            return require('codecompanion.adapters').extend('gemini', {
+              env = {
+                  api_key = os.getenv("GEMINI_API_KEY"),
+              },
+            })
+          end
+        '';
+      };
+    };
+
+    strategies = {
+      agent = {
+        adapter = "gemini";
+      };
+      chat = {
+        adapter = "gemini";
+      };
+      inline = {
+        adapter = "gemini";
       };
     };
   };
